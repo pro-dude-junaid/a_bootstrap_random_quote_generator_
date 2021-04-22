7baf2eaf403d0c3c1b19e957eebd55550b95779a
Junaid Siddique <j.siddique@icloud.com>
2021-04-22 20:34:24 +0530

    Add random quote generator

---
+++ quotes.js
@@ -0,0 +1,56 @@
+//this file contains all the quotes.
+
+const quoteBank = [
+  {
+     quote: "When the power of love overcomes, the love of power the world will know peace.",
+     author: "Jimi Hendrix"
+  },
+  {
+    quote: "If you wanna make the world a better place, take a look at yourself, then make that change.",
+    author: "Michael Jackson"
+  },
+  {
+    quote: "None but ourselves can free our minds.",
+    author: "Bob Marley"
+  },
+  {
+    quote: "The army teaches boys to think like men.",
+    author: "Elvis Presley"
+  },
+  {
+    quote: "An eye for eye only ends up making the whole world blind.",
+    author: "Mahatma Gandhi"
+  },
+  {
+    quote: "If the deaf have to hear, the sound has to be very loud.",
+    author: "Bhagat Singh"
+  },
+  {
+    quote: "Innovation distinguishes between a leader and a follower.",
+    author: "Steve Jobs"
+  },
+  {
+    quote: "All that glitters is not gold.",
+    author: "William Shakespeare"
+  },
+  {
+    quote: "Life is not fair; get used to it.",
+    author: "Bill Gates"
+  },
+  {
+    quote: "People won't have time for you if you are always angry or complaining.",
+    author: "Stephen Hawkins"
+  },
+  {
+    quote: "If you can't explain it simply, you don't understand it well enough.",
+    author: "Albert Einstein"
+  },
+  {
+    quote: "I can calculate the motion of heavenly bodies, but not the madness of people.",
+    author: "Isaac Newton"
+  },
+  {
+    quote: "An equation means nothing to me unless it expresses a thought of God.",
+    author: "Ramanujan"
+  }
+]
